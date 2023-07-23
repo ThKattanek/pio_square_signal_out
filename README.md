a4fdca7117bdaad7db1af753ce842fde1e2c27a7
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2023-07-23 20:54:12 +0200

    changed readme.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# pio_square_signal_out
- 
+# Pio Square Signal Output
+This is an example of an square signal output with a raspberry pico.
+## Pictures
+![pic01](doc/pio_square_signal_out_Steckplatine.png)
--- firmware/main.c
+++ firmware/main.c
@@ -1,25 +1,52 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
-#include "pico/binary_info.h"
-const uint LED_PIN = 25;
+#include <pico/stdlib.h>
+#include <hardware/pio.h>
 
-int main() 
-{
-    bi_decl(bi_program_description("This is a test binary."));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
-    stdio_init_all();
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    uint8_t var1 = 0;
-    while (1)
-    {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 1);
-        //puts("Hello World");
-        printf("TEST-%d\n",var1++);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-    }
-} 
+// Our assembled program:
+#include "square_out.pio.h"
+
+int main() {
+
+    // Pick one PIO instance arbitrarily. We're also arbitrarily picking state
+    // machine 0 on this PIO instance (the state machines are numbered 0 to 3
+    // inclusive).
+    PIO pio = pio0;
+
+    /// \tag::load_program[]
+    // Load the assembled program directly into the PIO's instruction memory.
+    // Each PIO instance has a 32-slot instruction memory, which all 4 state
+    // machines can see. The system has write-only access.
+    for (int i = 0; i < count_of(squarewave_program_instructions); ++i)
+        pio->instr_mem[i] = squarewave_program_instructions[i];
+    /// \end::load_program[]
+
+    /// \tag::clock_divider[]
+    // Configure state machine 0 to run at sysclk/2.5. The state machines can
+    // run as fast as one instruction per clock cycle, but we can scale their
+    // speed down uniformly to meet some precise frequency target, e.g. for a
+    // UART baud rate. This register has 16 integer divisor bits and 8
+    // fractional divisor bits.
+    pio->sm[0].clkdiv = (uint32_t) (31.25f * (1 << 16));
+    /// \end::clock_divider[]
+
+    /// \tag::setup_pins[]
+    // There are five pin mapping groups (out, in, set, side-set, jmp pin)
+    // which are used by different instructions or in different circumstances.
+    // Here we're just using SET instructions. Configure state machine 0 SETs
+    // to affect GPIO 0 only; then configure GPIO0 to be controlled by PIO0,
+    // as opposed to e.g. the processors.
+    pio->sm[0].pinctrl =
+        (1 << PIO_SM0_PINCTRL_SET_COUNT_LSB) |
+        (0 << PIO_SM0_PINCTRL_SET_BASE_LSB);
+    gpio_set_function(0, GPIO_FUNC_PIO0);
+    /// \end::setup_pins[]
+
+    /// \tag::start_sm[]
+    // Set the state machine running. The PIO CTRL register is global within a
+    // PIO instance, so you can start/stop multiple state machines
+    // simultaneously. We're using the register's hardware atomic set alias to
+    // make one bit high without doing a read-modify-write on the register.
+    hw_set_bits(&pio->ctrl, 1 << (PIO_CTRL_SM_ENABLE_LSB + 0));
+    /// \end::start_sm[]
+
+    return 0;
+}
